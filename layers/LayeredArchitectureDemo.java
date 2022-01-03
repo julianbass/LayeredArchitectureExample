2c585d7c52e5426ba113911f9df9dee1ab909380
Julian Bass <j.bass@salford.ac.uk>
2022-01-03 14:37:07 +0000

    Create java project and dummy main method

---
+++ layers/LayeredArchitectureDemo.java
@@ -0,0 +1,11 @@
+package layers;
+
+public class LayeredArchitectureDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Layered Architecture Demo");
+
+	}
+
+}
